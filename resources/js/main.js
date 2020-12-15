857bc7fa41dbe8348c31c6b9617a6a7249c686f2
Mahmudul Hasan Sajib <46852864+mhsajib@users.noreply.github.com>
2020-12-16 02:55:24 +0600

    Add files via upload

---
+++ resources/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  
+        $(".js--services-section").waypoint(function(direction){
+                if(direction == "down"){
+                      $("nav").addClass("sticky");
+                }else{
+                        $("nav").removeClass("sticky");
+
+                }
+        });
+
+        // mixitup----portfolio-section
+        var mixer = mixitup('.container');
+
+});
+
+function openNav(){
+        document.getElementById("myNav").style.width = "100%"
+}
+function closeNav(){
+        document.getElementById("myNav").style.width = "0%"
+}
